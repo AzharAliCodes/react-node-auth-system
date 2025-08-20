9ac10c3c12f465e672d3253db51c8bb926b596e5
AzharAliCodes <azharuddinali017@gmail.com>
2025-08-20 23:19:26 +0530

    db connected

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect("mongodb://localhost:27017/");
+    console.log("mongoDB Connected");
+  } catch (error) {
+    console.log(`Error occured ${error.message}`);
+    process.exit(1)
+  }
+};
+module.exports = connectDB;
