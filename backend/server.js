99a62df38d1dc1f70563a0e573e978c7a557816a
AzharAliCodes <azharuddinali017@gmail.com>
2025-08-24 18:27:58 +0530

    created server.js

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,7 @@ const authRoutes = require('./routes/authRoutes')
 const app = express();
 connectDB();
 
+app.use(express.json)
+
+const PORT = 5000; 
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
