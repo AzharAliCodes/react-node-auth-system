b1ade446302b76cdaa0b0424913317a0e1628cd3
AzharAliCodes <azharuddinali017@gmail.com>
2025-08-24 18:30:46 +0530

    soved error

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,6 @@ const app = express();
 connectDB();
 
 app.use(express.json)
-
+app.use('/api/auth',authRoutes)
 const PORT = 5000; 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
