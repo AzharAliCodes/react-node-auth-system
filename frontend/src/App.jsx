87e3b05fecd53baa7d8c095fe07cd3a2d5464e00
AzharAliCodes <azharuddinali017@gmail.com>
2025-08-27 14:46:07 +0530

    app jdx upated all route added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
+import {BrowserRouter as  Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Signup from "./pages/Signup"
+import Signin from "./pages/Signin"
+import PrivateRoute from "./components/PrivateRoute"
 
 function App() {
   return (
-    <Home/>
+    <Router>
+      <Routes>
+        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route 
+        path="/home"
+        element={
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>
+        }
+        />
+      </Routes>
+    </Router>
   )
 }
 
